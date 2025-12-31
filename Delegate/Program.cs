59ad740f358c4ba9a883e3d0c67123306a244668
fatimalikova <fatimakhm@code.edu.az>
2025-12-31 14:41:21 +0400

    Delegate added

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,14 +1,31 @@
-﻿namespace Delegate
+﻿
+namespace Delegate
 {
     internal class Program
     {
         public delegate bool NumberMethod(int number);
         static void Main(string[] args)
         {
-            SumOfNumbers((s) => s % 2==0, 1, 2, 3, 4, 5, 6);
-            SumOfNumbers((s) => s %2 ==1 , 1, 2, 3, 4, 5, 6);
+            //SumOfNumbers((s) => s % 2 == 0, 1, 2, 3, 4, 5, 6);
+            //SumOfNumbers((s) => s % 2 == 1 , 1, 2, 3, 4, 5, 6);
+            //SumOfNumbers(delegate (int s) { return s % 2 == 1; },
+            //    1, 2, 3, 4, 5, 6);
+
+
+            NumberMethod method = IsEven;
+            method += IsOdd;
+            SumOfNumbers(method, 1, 2, 3, 4, 5, 6);
         }
 
+        private static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        private static bool IsOdd(int number)
+        {
+            return number % 2 == 1;
+        }
 
         public static void SumOfNumbers(NumberMethod method, params int[] numbers)
         {
